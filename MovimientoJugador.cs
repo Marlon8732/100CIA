5efa09d0456d0743c1765af28339e885c7af0941
Marlon Davila <marlon_davil@yahoo.com>
2023-05-18 15:30:38 -0600

    Movimineto de izquierda a derecha y cambio de direccion de la animacion, par que este en la misma direccion que el movimiento

--- MovimientoJugador.cs
+++ MovimientoJugador.cs
@@ -4,34 +4,57 @@ using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
-      // Definir variables públicas para la velocidad y la fuerza del salto
-public float speed = 5f;
-public float jumpForce = 10f;
 
-// Obtener los componentes necesarios del personaje
-private Rigidbody2D rb2d;
-private Animator anim;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
 
-// Detectar si el personaje está tocando el suelo
-private bool isGrounded;
-public Transform ControladorSuelo;
-public float checkRadius;
-public LayerMask QueEsSuelo;
-// Detectar la dirección del movimiento
-private bool facingRight = true;
+    private bool isFacingRight = true;
+    private bool isJumping = false;
+    private Rigidbody2D rb;
 
-// Obtener la referencia a la cámara
-public Transform camTransform;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Movimiento horizontal
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+
+        // Girar el personaje
+        if (moveX < 0 && isFacingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveX > 0 && !isFacingRight)
+        {
+            FlipCharacter();
+        }
+
+        // Salto
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verificar si el personaje está en el suelo
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void FlipCharacter()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
