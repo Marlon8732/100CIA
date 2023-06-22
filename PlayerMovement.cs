b612a0fbf02e2af3f32c44b582060351e5e0345f
Marlon Davila <marlon_davil@yahoo.com>
2023-06-21 20:43:41 -0600

    se agrego controlador para activar transicion de animaciones cuando el valor del parámetro "Speed" sea mayor que cero, la transición se activará y cambiará del estado  "Saludar" a "correr"  Cuando el personaje salte, el valor del parámetro "Jump"  se establecerá en true, lo que activará la transición del estado   "Correr" al estado "Salto".

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -14,27 +14,40 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rigidbody2D;
     public Joystick joystick;
     public float jumpForce = 5;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (joystick.Vertical>=0.5F && isGrounded.IsGrounded)
+        if (joystick.Vertical >= 0.5F && isGrounded.IsGrounded)
         {
             jump();
+            // Establecer el parámetro "Jump" en true para activar la animación de salto
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            // Establecer el parámetro "Jump" en false para desactivar la animación de salto
+            animator.SetBool("Jump", false);
         }
        
         horizontalMove = joystick.Horizontal * runSpeedHorizontal;
 
-        transform.position += new Vector3(horizontalMove, verticalMove, 0) * Time.deltaTime*runSpeed; 
+        transform.position += new Vector3(horizontalMove, verticalMove, 0) * Time.deltaTime * runSpeed; 
+        
+        // Establecer el parámetro "Speed" en la magnitud de la velocidad horizontal para activar la animación de correr
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
     }
+
     public void jump()
     {
-        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,jumpForce),ForceMode2D.Impulse);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
     }
 }
